1338b21fdaf0f793f75b92666d8a6241c5b404f1
kwinin <kwinin@163.com>
2020-02-21 16:32:15 +0800

    fix(): config

--- src/database/database.provide.ts
+++ src/database/database.provide.ts
@@ -1,20 +1,13 @@
-
 import { Sequelize } from 'sequelize-typescript';
 import { Article } from '../article/article.entity';
 import { AdminModel } from '../admin/admin.entity';
+import * as config from 'config'
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
-        dialect: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: '123456',
-        database: 'community',
-      });
+      const sequelize = new Sequelize(config.get('mysql'));
       sequelize.addModels([Article, AdminModel]);
       await sequelize.sync({
         alter: true,
