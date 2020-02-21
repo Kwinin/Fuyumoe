eed5755305ec54977af787ccff4bcfb07b3330bb
kwinin <kwinin@163.com>
2020-02-21 15:52:40 +0800

    feature(): article

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,17 +1,24 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
+import { SessionGuard } from '../auth/session-guard';
 
 @Controller('article')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
-
   @Get('list')
   getList() {
     return this.articleService.getList();
   }
 
+  @UseGuards(SessionGuard)
   @Post('add')
   add(@Body() data){
-    return this.articleService
+    return this.articleService.add(data)
   }
+
+  @Get('getById')
+  getById(@Query() id) {
+    return this.articleService.getById(id)
+  }
+
 }
